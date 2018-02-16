dfd98993460aec6757acd424f43b7a3a9face410
tee-enn <35913748+tee-enn@users.noreply.github.com>
2018-02-16 20:49:56 +1100

    Add files via upload

--- HTML/js/SignUp2_ICan.js
+++ HTML/js/SignUp2_ICan.js
@@ -1,115 +1,30 @@
-alert("before import");
+$(document).ready(function() {
+    $('#ican_form').bootstrapValidator().on('success.form.bv', function(e) {
+        // Prevent form submission
+        e.preventDefault();
 
-import fetchJsonp from 'fetch-jsonp';
+        // Get the form instance
+        var $form = $(e.target);
 
-alert("1");
+        // Get the BootstrapValidator instance
+        var bv = $form.data('bootstrapValidator');
 
-export function buildUrl(id, sheetNum, mode) {
-  return `https://spreadsheets.google.com/feeds/${mode}/${id}/${sheetNum}/public/values?alt=json-in-script`;
-}
-
-// Generic fetch and parse function
-function fetchAndParse(id, sheetNum, type, parseEntries) {
-  if (id.length === 0) {
-    return Promise.reject(new Error('empty id'));
-  }
-  const url = buildUrl(id, sheetNum, type);
-  return new Promise((resolve, reject) => {
-    fetchJsonp(url).then(function (response) {
-      return response.json();
-    }).then(function (json) {
-      const data = parseEntries(json.feed.entry);
-      const res = {
-        title: json.feed.title.$t,
-        updated: json.feed.updated.$t,
-        data
-      };
-      resolve(res);
-    }).catch(function (ex) {
-      reject(ex);
+        // Use Ajax to submit form data
+        var url = 'https://script.google.com/macros/s/AKfycbwsyZ7oxE6dm-ddj5EvCV39Csuc0Oj-6RS2pjJyV5taxAaxqDM/exec';
+        var redirectUrl = 'SignUp3_Ilearn.html';
+        // show the loading 
+        $('#postForm').prepend($('<span></span>').addClass('glyphicon glyphicon-refresh glyphicon-refresh-animate'));
+        var jqxhr = $.post(url, $form.serialize(), function(data) {
+            console.log("Success! Data: " + data.statusText);
+            $(location).attr('href',redirectUrl);
+        })
+            .fail(function(data) {
+                console.warn("Error! Data: " + data.statusText);
+                // HACK - check if browser is Safari - and redirect even if fail b/c we know the form submits.
+                if (navigator.userAgent.search("Safari") >= 0 && navigator.userAgent.search("Chrome") < 0) {
+                    //alert("Browser is Safari -- we get an error, but the form still submits -- continue.");
+                    $(location).attr('href',redirectUrl);                
+                }
+            });
     });
-  });
-}
-
-export function parseRawCells(entries) {
-  const data = [];
-  entries.forEach(cell => {
-    const row = parseInt(cell.gs$cell.row, 10) - 1;
-    const col = parseInt(cell.gs$cell.col, 10) - 1;
-    const content = cell.gs$cell.$t;
-    if (data[row] === undefined) {
-      data[row] = [];
-    }
-    data[row][col] = content;
-  });
-  return data;
-}
-
-/**
- * Use for table with labels only for columns
- */
-function parseEntry(entry) {
-  const res = {};
-  Object.keys(entry).forEach(key => {
-    if (key.indexOf('gsx$') === 0) {
-      const label = key.substr(4);
-      res[label] = entry[key].$t;
-    }
-  });
-  return res;
-}
-
-/**
- * Parser for table where just the columns are labeled
- * @return array of objects where the labels are keys
- */
-export function parseLabeledCols(entries) {
-  return entries.map(entry => parseEntry(entry));
-}
-
-/**
- * Use for table with labels for rows AND columns
- * Example input: "bar: 123, baz: 122, bab: 234"
- */
-function parseLabeledRow(row) {
-  const cols = row.split(', ');
-  const res = cols.reduce((acc, col) => {
-    const pair = col.split(': ');
-    acc[pair[0]] = pair[1];
-    return acc;
-  }, {});
-  return res;
-}
-
-/**
- * Parser for table where both rows and columns are labeled
- * @return object where keys are row labels and values are objects where keys are the column labels
- */
-export function parseLabeledRowsCols(entries) {
-  const res = {};
-  entries.forEach(entry => {
-    res[entry.title.$t] = parseLabeledRow(entry.content.$t);
-  });
-  return res;
-}
-
-// fetch as raw arrays
-export function raw(id, sheetNum = 1) {
-  return fetchAndParse(id, sheetNum, 'cells', parseRawCells);
-}
-
-// fetch as array of labeled columns
-export function labeledCols(id, sheetNum = 1) {
-  return fetchAndParse(id, sheetNum, 'list', parseLabeledCols);
-}
-
-// fetch as labeled map of labeled columns
-export function labeledColsRows(id, sheetNum = 1) {
-  return fetchAndParse(id, sheetNum, 'list', parseLabeledRowsCols);
-}
-
-export default {
-  raw,
-  labeledCols,
-  labeledColsRows
-};
+});
